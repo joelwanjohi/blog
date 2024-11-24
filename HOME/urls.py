ff94ca0de0427be968d5f3aaab26c39de68b6d00
joelwanjohi <joelwanjohi207@gmail.com>
2024-11-24 17:08:18 +0300

    py migrati

--- HOME/urls.py
+++ HOME/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+
+from Blog import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
  
     path("", views.index, name="index"),
     path("<str:slug>", views.blog_detail, name='blog_detail'),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
