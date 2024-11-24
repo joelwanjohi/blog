45cb48bf4417dfa41470231cb1bdf0063e54ef99
joelwanjohi <joelwanjohi207@gmail.com>
2024-11-24 16:19:43 +0300

    first com

--- HOME/models.py
+++ HOME/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from = "name", unique = True , null=True, default=None)
+
+    def save (self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            self.slug = f"{base_slug}"
+        super().save(*args, **kwargs)
+    def __str__(self) -> str:
+        return self.name
